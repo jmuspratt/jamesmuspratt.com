ae4011d1112fab79132432e0bf91b7fbb92c1303
James Muspratt <james@jamesmuspratt.com>
2019-09-28 16:47:30 -0400

    Hover clip effect working

--- src/_assets/js/index.js
+++ src/_assets/js/index.js
@@ -2,6 +2,29 @@ import $ from 'jQuery';
 window.$ = window.jQuery = $
 
 
-$(document).ready(()=>{
-    $('body').addClass('js');
-})
+const hoverClipPath = {
+    init() {
+        console.log('init');
+        const $layer2 = $('.layer--2'); 
+        const $contnet = $('.content');
+        $content.mousemove( (event)=>{
+            this.moveClipPath(event.pageX, event.pageY, $layer2);
+        });
+    },
+
+    moveClipPath(x, y, $layer) {
+        console.log('moving', x);
+        console.log('layer', $layer);
+        const clipPath = `polygon(0px 900px, ${x*1.3}px 0px, 0px 0px)`;
+        $layer.css({
+            "clip-path": clipPath,
+            "-webkit-clip-path": clipPath
+        });
+    },
+
+};
+
+$(document).ready(() => hoverClipPath.init());
+
+
+
