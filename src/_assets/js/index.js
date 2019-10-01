c3f2e9a3a7ce454cba791cd12185d3ee8e1da144
James Muspratt <james@jamesmuspratt.com>
2019-10-01 09:58:24 -0400

    Finish mask effect

--- src/_assets/js/index.js
+++ src/_assets/js/index.js
@@ -4,18 +4,26 @@ window.$ = window.jQuery = $
 
 const hoverClipPath = {
     init() {
-        console.log('init');
+        this.duplicateLayer();
+
         const $layer2 = $('.layer--2'); 
-        const $contnet = $('.content');
+        const $content = $('.content');
+
         $content.mousemove( (event)=>{
             this.moveClipPath(event.pageX, event.pageY, $layer2);
         });
     },
 
+    duplicateLayer() {
+        $('.layer--1')
+        .clone()
+        .removeClass('layer--1')
+        .addClass('layer--2')
+        .insertAfter('.layer--1');
+    },
+
     moveClipPath(x, y, $layer) {
-        console.log('moving', x);
-        console.log('layer', $layer);
-        const clipPath = `polygon(0px 900px, ${x*1.3}px 0px, 0px 0px)`;
+        const clipPath = `polygon(0px 100vh, ${x*1.3}px 0px, 0px 0px)`;
         $layer.css({
             "clip-path": clipPath,
             "-webkit-clip-path": clipPath
